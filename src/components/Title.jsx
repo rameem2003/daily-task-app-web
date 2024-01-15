901f2ef26a476d11abffc99b637a664e20269384
rameem2003 <rameem2019@gmail.com>
2024-01-15 08:22:50 +0600

    update

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,11 @@ const Title = () => {
   return (
     <div className=" w-full h-16 p-3 bg-primary flex items-center">
       <h1 className=" font-roboto font-semibold text-white text-2xl">
-        Daily Task <span className=" text-gray-300 text-[15px]"> v 0.0.1</span>
+        Daily Task{" "}
+        <span className=" text-gray-300 text-[15px]">
+          {" "}
+          v 0.0.1 Developer Preview
+        </span>
       </h1>
     </div>
   );
